c3b5a3ca67c47864b0ce457e5686832d41d9730b
Yeffry-Fermin <anthonybatista291@gmail.com>
2024-07-26 21:12:11 -0400

    add solution for Part 1-Exercise 2

--- Part-01/part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
+++ Part-01/part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -3,6 +3,6 @@ public class AdaLovelace {
 
     public static void main(String[] args) {
         // Write your program here
-        System.out.println("Hello My first Java Program :D");
+        System.out.println("Ada Lovelace");
     }
 };
