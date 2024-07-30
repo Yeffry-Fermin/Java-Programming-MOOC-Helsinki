793675f77eb91c29a9b5d0a9d2ce6ab70af79199
Yeffry-Fermin <anthonybatista291@gmail.com>
2024-07-30 19:06:28 -0400

    add solution for exercise 01-16

--- Part-01/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ Part-01/part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -7,6 +7,12 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("How many days would you like to convert to seconds?");
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        int secondsInDay = (number * 86400);
+        
+        System.out.println(secondsInDay);
     }
 }
