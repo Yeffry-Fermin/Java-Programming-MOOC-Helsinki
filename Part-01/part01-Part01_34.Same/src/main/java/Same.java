fb08abfaf57b4c28a83dc466453ff9e9710e9009
Yeffry-Fermin <anthonybatista291@gmail.com>
2024-08-04 16:12:39 -0400

    add fix for solution for exercise 01-34

--- Part-01/part01-Part01_34.Same/src/main/java/Same.java
+++ Part-01/part01-Part01_34.Same/src/main/java/Same.java
@@ -6,6 +6,13 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
-    }
+        String first = scan.nextLine();
+        String second = scan.nextLine();
+        
+        if(first.equals(second)) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
+    }  
 }
